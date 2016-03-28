1e8a7ec43fb21324ae7ae60e9fe2fc64d2c6dfca
Jason Andrew Paredes <Jason Andrew Paredes>
2016-03-28 21:00:48 +0800

    database

--- config/database.php
+++ config/database.php
@@ -33,8 +33,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
-//	  'default' => 'pgsql',
+//    'default' => env('DB_CONNECTION', 'mysql'),
+	  'default' => 'pgsql',
 
     /*
     |--------------------------------------------------------------------------
@@ -74,13 +74,13 @@ return [
 
         'pgsql' => [
             'driver'   => 'pgsql',
-            'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'forge'),
-            'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD', ''),
-            'charset'  => 'utf8',
-            'prefix'   => '',
-            'schema'   => 'public',
+    'host'     => parse_url(getenv("DATABASE_URL"))["host"],
+    'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
+    'username' => parse_url(getenv("DATABASE_URL"))["user"],
+    'password' => parse_url(getenv("DATABASE_URL"))["pass"],
+    'charset'  => 'utf8',
+    'prefix'   => '',
+    'schema'   => 'public',
         ],
 
         'sqlsrv' => [
